a73749e2b936d3c26881ceb521e65b4bc20e822e
Alan Navarro <alanjnavarrom@gmail.com>
2017-11-03 19:43:31 -0500

    Added save and mix functionality.

--- hex.js
+++ hex.js
@@ -5,6 +5,9 @@ inventory.green = [0, 255, 0];
 inventory.blue = [0, 0, 255];
 inventory.white = [255, 255, 255];
 inventory.black = [0, 0, 0];
+inventory.yellow = [255, 255, 0];
+inventory.cyan = [0, 255, 255];
+inventory.magenta = [255, 0, 255];
 
 var viewBox = document.getElementById("viewBox"); //color output
 var messageBox = document.getElementById("messageBox"); //message output
@@ -32,7 +35,7 @@ function mixNum(){
     var mixNew = []
     var mv1 = inventory[mi1.value]; //turns string to list [n, n, n]
     var mv2 = inventory[mi2.value];
-    var n = 1; //add then overflow
+    var n = 2; //add then overflow
     mixNew.push(Math.round((mv1[0]+mv2[0])/n));
     mixNew.push(Math.round((mv1[1]+mv2[1])/n));
     mixNew.push(Math.round((mv1[2]+mv2[2])/n));
@@ -50,12 +53,16 @@ var nameInput = document.getElementById("nameInput");
 var inventoryBox = document.getElementById("inventoryBox");
 
 function saveName(){ //fix the object.values thing. find values in inventory
-  dev.innerHTML = "ovi is: " + Object.values(inventory) + "and currentColor is: " + currentColor;
+  dev.innerHTML = "The current color is: " + currentColor;
   if (currentColor === undefined) {
     messageBox.innerHTML = "You need to mix a color first";
   } else if (nameInput.value in inventory) {
     messageBox.innerHTML = "You already have a color named " + nameInput.value + " in your inventory";
   } else if (currentColor in Object.values(inventory)) {
-    messageBox.innerHTML = "Current color in inventory.values() and its: " + currentColor
-  };
+    messageBox.innerHTML = "Current color in inventory.values() and its: " + currentColor;
+  } else {
+    // Save the color.
+    inventory[nameInput.value]  = currentColor;
+    inventoryBox.innerHTML = inventoryBox.innerHTML + "," + nameInput.value;
+  }
 };
