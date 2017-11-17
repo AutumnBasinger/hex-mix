6c5bfaa7ae0f223371cc8b8d6b99b784f7bce9bc
Autumn Basinger <autumnelise@hotmail.com>
2017-11-16 20:19:44 -0800

    Tiles skeleton

--- hex.js
+++ hex.js
@@ -1,83 +1,55 @@
-//initial default inventory
-var inventory = {};
-inventory.red = [255, 0, 0];
-inventory.green = [0, 255, 0];
-inventory.blue = [0, 0, 255];
-inventory.white = [255, 255, 255];
-inventory.black = [0, 0, 0];
+var tube = [[255,0,255], [255,255,0], [0,255,255]]
+var pallete = [undefined, undefined, undefined]
+var canvas = [undefined, undefined, undefined]
 
-var viewBox = document.getElementById("viewBox"); //color output
-var messageBox = document.getElementById("messageBox"); //message output
-var viewInput = document.getElementById("viewInput"); //color input string
-var dev = document.getElementById("dev"); //debug message
-var currentColor;
+function makeTube(name, value){
+  var name = document.getElementById(string(name));
+  var value = value
+}
 
-function viewNum(){
-  if (viewInput.value in inventory) { //color stringname
-    currentColor = inventory[viewInput.value]; //rgb array
-    viewBox.style.backgroundColor = "rgb(" + currentColor + ")";
-    viewBox.innerHTML = "String here so color shows, fix later"
-    messageBox.innerHTML = "You're looking at the color " + currentColor;
-    dev.innerHTML = "Dev: currentColor variable = " + currentColor;
+function update(tile, clear){
+  if (clear) {
+    tile.style.backgroundColor = undefined;
   } else {
-    messageBox.innerHTML = "You don't have that color yet";
+    tile.value = "new value"
+    tile.style.backgroundColor = "rgb(" + "new value" + ")";
   };
 };
 
-var mi1 = document.getElementById("mi1"); //string
-var mi2 = document.getElementById("mi2"); //string
+function getColor(tile){
+  //return tile.value
+}
 
-function mixNum(){
-  if (mi1.value in inventory && mi2.value in inventory) { //color stringnames
-    var mixNew = []
-    var mv1 = inventory[mi1.value]; //turns string to rgb array
-    var mv2 = inventory[mi2.value];
-    var n = 1; //try add then overflow
-    mixNew.push(Math.round((mv1[0]+mv2[0])/n));
-    mixNew.push(Math.round((mv1[1]+mv2[1])/n));
-    mixNew.push(Math.round((mv1[2]+mv2[2])/n));
-    currentColor = mixNew;
-    messageBox.innerHTML = "You just mixed the color: " + currentColor; //rgb array
-    viewBox.style.backgroundColor = "rgb(" + currentColor + ")";
-    viewBox.innerHTML = "String here so color shows, fix later";
-    dev.innerHTML = "Dev: currentColor variable = " + currentColor;
-  } else {
-    messageBox.innerHTML = "You don't have at least one of those colors yet"
-  };
-};
+function drag(t1, t2){
+  //getColor(t1)
+  //getColor(t2)
+  //anything + tube = error()
+  //anything + pallete = calculate()
+  //anything + canvas = arrange()
+}
 
-var nameInput = document.getElementById("nameInput");
-var inventoryBox = document.getElementById("inventoryBox");
+function error(){
+  //display eror
+}
 
-function inventoryValues(){
-  var inventoryValues = []
-  for (var key in inventory){
-    inventoryValues.push(inventory[key]);
-    return inventoryValues;
-  };
-};
+function calculate(){
+  //calculate and display color
+  //var mixNew = []
+  //mixNew.push(Math.round((mv1[0]+mv2[0])/n));
+  //mixNew.push(Math.round((mv1[1]+mv2[1])/n));
+  //mixNew.push(Math.round((mv1[2]+mv2[2])/n));
+}
 
-//checks if currentColor (color shown) in colors list
-function valueInInventory() {
-  var i = inventoryValues().length;
-  while (i--) {
-    if (inventoryValues()[i] === currentColor) {
-      return true;
-    };
-  };
-  return false;
-};
+function arrange(){
+  //update arrangement
+}
 
-function saveName(){
-  if (currentColor === undefined) {
-    messageBox.innerHTML = "You need to mix a color first";
-  } else if (nameInput.value in inventory) {
-    messageBox.innerHTML = "You already have a color named " + nameInput.value + " in your inventory";
-  } else if (valueInInventory()) {
-    messageBox.innerHTML = "Do you want to change this color's name to " + nameInput.value + " ?";
-  } else {
-    inventory[nameInput.value] = currentColor;
-    messageBox.innerHTML = "You just added the color " + nameInput.value + " to your inventory!";
-    inventoryBox.innerHTML = Object.keys(inventory);
-  };
-};
+var m = document.getElementById("m");
+var y = document.getElementById("y");
+var c = document.getElementById("c");
+var p1 = document.getElementById("p1");
+var p2 = document.getElementById("p2");
+var p3 = document.getElementById("p3");
+var c1 = document.getElementById("c1");
+var c2 = document.getElementById("c2");
+var c3 = document.getElementById("c3");
