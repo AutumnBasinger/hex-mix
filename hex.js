61c0f401121bd6e96f03edf04a28f004aae4a9ef
Autumn Basinger <autumnelise@hotmail.com>
2018-01-15 16:26:07 -0800

    Add time to color functions

--- hex.js
+++ hex.js
@@ -1,5 +1,32 @@
 let colorOutput = document.getElementById('colorOutput');
 
-function timeToColor(currentTime) {
-  colorOutput.style.backgroundColor = "rgb(" + [0, 255, 255] + ")";
+var time = new Date();
+hour = time.getHours();
+minute = time.getMinutes();
+second = time.getSeconds();
+console.log(hour, minute, second);
+
+function secondToColor() {
+  newSecond = Math.round(second * 4.26);
+  return newSecond;
 }
+
+function minuteToColor() {
+  newMinute = Math.round((minute * 4.26) + (secondToColor()/60))
+  return newMinute;
+}
+
+function hourToColor() {
+  newHour = Math.round((hour * 10.66) + (minuteToColor()/60) + (secondToColor()/60/60));
+  console.log(newHour, newMinute, newSecond);
+  return newHour;
+}
+
+function showColor() {
+  colorOutput.style.backgroundColor = "rgb(" + [newHour, newMinute, newSecond] + ")";
+}
+
+secondToColor();
+minuteToColor();
+hourToColor();
+showColor();
