d676b7c7da7f99a6628994f0bfcb65fca6e276db
Autumn Basinger <autumnelise@hotmail.com>
2018-01-15 17:31:19 -0800

    Add setInterval function

--- hex.js
+++ hex.js
@@ -1,32 +1,18 @@
 let colorOutput = document.getElementById('colorOutput');
 
-var time = new Date();
-hour = time.getHours();
-minute = time.getMinutes();
-second = time.getSeconds();
-console.log(hour, minute, second);
+function main() {
+  let time = new Date();
+  hour = time.getHours();
+  minute = time.getMinutes();
+  second = time.getSeconds();
 
-function secondToColor() {
-  newSecond = Math.round(second * 4.26);
-  return newSecond;
+  red = Math.round(second * 4.26);
+  green = Math.round((minute * 4.26) + (red/60));
+  blue = Math.round((hour * 10.66) + (green/60));
+
+  colorOutput.style.backgroundColor = "rgb(" + [red, green, blue] + ")";
 }
 
-function minuteToColor() {
-  newMinute = Math.round((minute * 4.26) + (secondToColor()/60))
-  return newMinute;
-}
+main();
 
-function hourToColor() {
-  newHour = Math.round((hour * 10.66) + (minuteToColor()/60) + (secondToColor()/60/60));
-  console.log(newHour, newMinute, newSecond);
-  return newHour;
-}
-
-function showColor() {
-  colorOutput.style.backgroundColor = "rgb(" + [newHour, newMinute, newSecond] + ")";
-}
-
-secondToColor();
-minuteToColor();
-hourToColor();
-showColor();
+let timerID = setInterval(main, 1000);
