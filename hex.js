c72df645b2cb0c0f68168f5590590e82f3dfd7be
Autumn Basinger <autumnelise@hotmail.com>
2018-02-13 22:05:12 -0800

    Reorganize functions

--- hex.js
+++ hex.js
@@ -1,38 +1,32 @@
-let canvas = document.getElementById('canvas');
-canvas.width = 1200;
-canvas.height = 1200;
-let ctx = canvas.getContext('2d');
-ctx.scale(6,6);
-
-let rgbTime = document.getElementById('rgbTime');
 let realTime = document.getElementById('realTime');
+let canvas = document.getElementById('canvas');
+let rgbTime = document.getElementById('rgbTime');
 
-function drawCircle(x,y,radius,center,color){
+canvas.width = 1200; canvas.height = 1200;
+let ctx = canvas.getContext('2d'); ctx.scale(6,6);
+
+//divisions, which divison (0 is 12:00), distance from center
+function position(div,pos,ofs) {
+  pos = pos-div/4;
+  let deg = 360/div;
+  let rad = (pos*deg*2*Math.PI)/360;
+  let x = ofs*Math.cos(rad)+100;
+  let y = ofs*Math.sin(rad)+100;
+  return [x,y];
+}
+
+function drawCircle(x,y,radius,color){
   ctx.beginPath();
-  ctx.arc(x,y,radius,center,2*Math.PI);
+  ctx.arc(x,y,radius,0,2*Math.PI);
   ctx.fillStyle = "rgb(" + color + ")"
   ctx.fill();
 }
 
-function positionCircle(divisions, position, color, offset, size) {
-  position = position-divisions/4;
-  let deg = 360/divisions;
-  let rad = (position*deg*2*Math.PI)/360;
-  let x = offset * Math.cos(rad) + 100;
-  let y = offset * Math.sin(rad) + 100;
-  drawCircle(x,y,size,0,color);
-}
-
-function positionLine(divisions, position, offset, color) {
-  position = position-divisions/4;
-  let deg = 360/divisions;
-  let rad = (position*deg*2*Math.PI)/360;
-  let x = offset * Math.cos(rad) + 100;
-  let y = offset * Math.sin(rad) + 100;
+function lineCenterTo(x,y,width,color) {
   ctx.beginPath();
   ctx.moveTo(100,100);
   ctx.lineTo(x,y);
-  ctx.lineWidth = 1;
+  ctx.lineWidth = width;
   ctx.strokeStyle = "rgb(" + color + ")";
   ctx.stroke();
 }
@@ -44,31 +38,28 @@ function main() {
   currentHour = time.getHours();
   totalSec = currentSec + currentMin*60 + currentHour*60*60
 
-  let tick = 256/60; //change in RGB value per 1 cycle out of 60
-  let tock = 256/24; //change in RGB value per 1 cycle out of 24
-
+  let tick = 256/60; //change in RGB per 1 cycle out of 60
+  let tock = 256/24; //change in RGB per 1 cycle out of 24
   rgbSec = Math.round(tick*currentSec);
   rgbMin = Math.round(tick*currentMin + rgbSec/60);
   rgbHour = Math.round(tock*currentHour + rgbMin/60);
-
   let rgb = [rgbHour, rgbMin, rgbSec];
-  let white = [255,255,255]
+  let white = [255,255,255];
 
-  if (totalSec <= 43200){ //43200 is half the number of seconds in a day
+  if (totalSec <= 43200){ //half of 86400 seconds in a day
     size = (totalSec/432);
   } else {
     size = Math.abs(100 - (totalSec/432 - 100))
   }
 
   realTime.innerHTML = currentHour + ':' + currentMin + ':' + currentSec;
-  realTime.style.color = "rgb(" + white  + ")";
-  drawCircle(100,100,100,0,white); //refresh circle, 100 r is max canvas
-  drawCircle(100,100,size,0,rgb); //main circle
-  positionLine(12, currentHour, size, white);
-  positionCircle(12, currentHour, rgb, size+size/4, size/4)
-  document.body.style.backgroundColor = "rgb(" + rgb  + ")";
   rgbTime.innerHTML = rgbHour + ':' + rgbMin + ':' + rgbSec;
-  rgbTime.style.color = "rgb(" + white  + ")";
+  document.body.style.backgroundColor = "rgb(" + rgb  + ")";
+
+  drawCircle(100,100,100,white);
+  drawCircle(100,100,size,rgb);
+  let hour = position(12,currentHour,size);
+  lineCenterTo(hour[0],hour[1],1,white);
 }
 
 main();
